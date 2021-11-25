79e17e8326e59ebe4a55c13e9129ed31656ddd71
Kevin Kennis <kevin@pawn.finance>
2021-11-24 23:58:43 -0800

    show laons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
-import logo from './logo.svg';
+import { useWallet, UseWalletProvider } from 'use-wallet';
+import { ethers } from "ethers";
+import moment from 'moment';
+
+import MMLogo from './mm_logo.svg';
 import './App.css';
 
+import { usePawnLender } from './use-pawn-lender';
+
+function Main() {
+  return (
+    <UseWalletProvider>
+      <App />
+    </UseWalletProvider>
+  );
+}
+
 function App() {
+  const wallet = useWallet();
+  const chainInfo = usePawnLender(wallet);
+
+  window.chainInfo = chainInfo;
+
+  if (wallet && wallet.chainId !== 1 && wallet.chainId !== 1337) {
+    return (
+      <div className="App">
+        <div className="container main">
+          <h1>Pawn.fi Rollover Signer</h1>
+          <hr />
+          <h4 className="bold header">Please switch your wallet network to Ethereum mainnet.</h4>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container main">
+        <h1 className="header">Pawn.fi Rollover Signer</h1>
+        <hr />
+        {wallet.status === 'connected' ?
+          <SignerContainer wallet={wallet} chainInfo={chainInfo} />
+          :
+          <ConnectPrompt wallet={wallet} />
+        }
+
+      </div>
     </div>
   );
 }
 
-export default App;
+function SignerContainer({ chainInfo, wallet }) {
+  if (!chainInfo) return <h4>Loading...</h4>;
+
+  return (
+    <div>
+      <h4>Active Loans</h4>
+      {chainInfo.loans.map((loan, i) => {
+        const { loanId, terms, data } = loan;
+
+        return (
+          <div className="loan card" key={i}>
+            <h5 className="bold">
+              Loan ID {loanId.toString()}
+              {loan.legacy && ` (Legacy)`}
+            </h5>
+            <p>
+              <strong>Principal:</strong>
+              {' '}
+              {ethers.utils.formatUnits(terms.principal, terms.payableTokenDecimals)}
+              {' '}
+              {terms.payableTokenSymbol}
+            </p>
+            <p>
+              <strong>Interest:</strong>
+              {' '}
+              {ethers.utils.formatUnits(terms.interest, terms.payableTokenDecimals)}
+              {' '}
+              {terms.payableTokenSymbol}
+            </p>
+            <CollateralList loanId={loanId} collateral={loan.collateral} />
+            <p><strong><DueDate ts={data.dueDate} /></strong></p>
+            <p><strong><a className='rollover toggle'>Rollover Loan</a></strong></p>
+          </div>
+        );
+      })}
+    </div>
+  )
+}
+
+function CollateralList({ loanId, collateral }) {
+  return (
+    <div>
+      <strong>Collateral:</strong>
+      <ul className='collateral list'>
+        {collateral.erc20.map(c =>
+          <li key={loanId + c.tokenAddress}>
+            {ethers.utils.formatUnits(c.amount, c.decimals)} {c.symbol}
+          </li>
+        )}
+        {collateral.erc721.map(c =>
+          <li key={loanId + c.tokenAddress}>
+            {c.tokenName} #{c.tokenId.toNumber()}
+          </li>
+        )}
+        {collateral.erc1155.map(c =>
+          <li key={loanId = c.tokenAddress}>
+            {c.amount} of {c.tokenName} #{c.tokenId}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+function DueDate({ ts }) {
+  const d = moment.unix(ts);
+
+  if (d > Date.now()) {
+    return `Due ${d.fromNow()} (${d.format('LLL')})`;
+  } else {
+    return `Overdue: due ${d.toNow()} (${d.format('LLL')})`;
+  }
+}
+
+function SigningForm() {
+  return <></>;
+}
+
+function ConnectPrompt({ wallet }) {
+  return (
+    <div className='centered'>
+      <button className='connect' onClick={() => wallet.connect()}>
+        <img src={MMLogo} alt="Metamask" className='mm-logo' />
+        &nbsp;
+        Connect Metamask
+      </button>
+    </div>
+  );
+}
+
+window.instructions = () => {
+  const str = `
+Flash Rollover Prerequisites:
+
+1. Make sure borrower has approved BorrowerNote for withdrawal by flash rollover contract
+2. Make sure lender has approved OriginationController to withdraw tokens
+3. Make sure lender has approved FlashRollover to withdraw tokens
+`;
+
+  console.log(str);
+}
+
+export default Main;
---
+++ src/use-pawn-lender.js
@@ -0,0 +1,318 @@
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+import LegacyPromissoryNoteAbi from './abis/legacy-promissory-note.json';
+import LegacyLoanCoreAbi from './abis/legacy-loan-core.json';
+import PromissoryNoteAbi from './abis/promissory-note.json';
+import LoanCoreAbi from './abis/loan-core.json';
+import AssetWrapperAbi from './abis/asset-wrapper.json';
+import FlashRolloverAbi from './abis/flash-rollover.json';
+import ERC20Abi from './abis/erc20.json';
+import ERC721Abi from './abis/erc721.json';
+import ERC1155Abi from './abis/erc1155.json';
+
+export const addresses = {
+    legacy: {
+        lenderNote: '0xD96e4D03420aA33a3FE91f57D03D8Ef69dE1e863',
+        loanCore: '0x59e57F9A313A2EB1c7357eCc331Ddca14209F403'
+    },
+    current: {
+        lenderNote: '0x6BD1476dD1D57f08670AF6720CA2eDf37B10746E',
+        loanCore: '0x606E4a441290314aEaF494194467Fd2Bb844064A'
+    },
+    common: {
+        assetWrapper: '0x1F563CDd688ad47b75E474FDe74E87C643d129b7',
+        flashRollover: '0x24611Fad669350cA869FBed4B62877d1a409dA12'
+    },
+    tokens: {
+        usdc: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+        weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+    }
+}
+
+export function usePawnLender(wallet) {
+    window.wallet = wallet;
+
+    const [chainInfo, setChainInfo] = useState(null);
+
+    useEffect(() => {
+        const getInfo = async () => {
+            if (!wallet?.account) return;
+
+            const { account } = wallet;
+            const provider = new ethers.providers.Web3Provider(wallet.ethereum);
+
+            // Find active loans
+            // For lender note, check owned tokens - for each one, get the loan ID
+            const legacyLenderNote = new ethers.Contract(addresses.legacy.lenderNote, LegacyPromissoryNoteAbi, provider);
+            const lenderNote = new ethers.Contract(addresses.current.lenderNote, PromissoryNoteAbi, provider);
+            const legacyLoanCore = new ethers.Contract(addresses.legacy.loanCore, LegacyLoanCoreAbi, provider);
+            const loanCore = new ethers.Contract(addresses.current.loanCore, LoanCoreAbi, provider);
+            const assetWrapper = new ethers.Contract(addresses.common.assetWrapper, AssetWrapperAbi, provider);
+            const flashRollover = new ethers.Contract(addresses.common.flashRollover, FlashRolloverAbi, provider);
+
+            const usdc = new ethers.Contract(addresses.tokens.usdc, ERC20Abi, provider);
+            const weth = new ethers.Contract(addresses.tokens.weth, ERC20Abi, provider);
+
+            const ethBalance = await provider.getBalance(account);
+            const usdcBalance = await usdc.balanceOf(account);
+            const wethBalance = await weth.balanceOf(account);
+
+            const numLenderNotes = await lenderNote.balanceOf(account);
+            const numLegacyLenderNotes = await legacyLenderNote.balanceOf(account);
+
+            const loans = [];
+
+            for (let i = 0; i < numLenderNotes; i++) {
+                const lenderNoteId = await lenderNote.tokenOfOwnerByIndex(account, i);
+                const loanId = await lenderNote.loanIdByNoteId(lenderNoteId);
+                const loanData = await loanCore.getLoan(loanId);
+
+                // Active state
+                if (loanData.state === 2) {
+                    // Find collateral
+                    const tokenId = loanData.terms.collateralTokenId;
+                    const erc20Holdings = [];
+                    const erc721Holdings = [];
+                    const erc1155Holdings = [];
+
+                    let failed = false;
+                    let i = 0;
+                    while (!failed) {
+                        try {
+                            const holding = await assetWrapper.bundleERC20Holdings(tokenId, i++);
+                            const token = new ethers.Contract(holding.tokenAddress, ERC20Abi, provider);
+                            const tokenName = await token.name();
+                            const symbol = await token.symbol();
+                            const decimals = await token.decimals();
+
+                            erc20Holdings.push({
+                                amount: holding.amount,
+                                tokenAddress: holding.tokenAddress,
+                                token,
+                                tokenName,
+                                symbol,
+                                decimals
+                            });
+                        } catch (e) {
+                            failed = true;
+                        }
+                    }
+
+                    failed = false;
+                    i = 0;
+                    while (!failed) {
+                        try {
+                            const holding = await assetWrapper.bundleERC721Holdings(tokenId, i++);
+                            const token = new ethers.Contract(holding.tokenAddress, ERC721Abi, provider);
+                            const tokenName = await token.name();
+                            const symbol = await token.symbol();
+
+                            erc721Holdings.push({
+                                tokenAddress: holding.tokenAddress,
+                                tokenId: holding.tokenId,
+                                token,
+                                tokenName,
+                                symbol
+                            });
+                        } catch (e) {
+                            failed = true;
+                        }
+                    }
+
+                    failed = false;
+                    i = 0;
+                    while (!failed) {
+                        try {
+                            const holding = await assetWrapper.bundleERC1155Holdings(tokenId, i++);
+                            const token = new ethers.Contract(holding.tokenAddress, ERC1155Abi, provider);
+                            const tokenName = await token.name();
+                            const symbol = await token.symbol();
+
+                            erc1155Holdings.push({
+                                tokenAddress: holding.tokenAddress,
+                                tokenId: holding.tokenId,
+                                amount: holding.amount,
+                                token,
+                                tokenName,
+                                symbol
+                            });
+                        } catch (e) {
+                            failed = true;
+                        }
+                    }
+
+                    const payableToken = new ethers.Contract(loanData.terms.payableCurrency, ERC20Abi, provider);
+
+                    loans.push({
+                        legacy: false,
+                        loanId: loanId,
+                        data: {
+                            state: loanData.state,
+                            dueDate: loanData.dueDate.toNumber(),
+                            borrowerNoteId: loanData.borrowerNoteId.toNumber(),
+                            lenderNoteId: loanData.lenderNoteId.toNumber()
+                        },
+                        terms: {
+                            collateralTokenId: loanData.terms.collateralTokenId,
+                            payableCurrency: loanData.terms.payableCurrency,
+                            payableToken: payableToken,
+                            payableTokenName: await payableToken.name(),
+                            payableTokenSymbol: await payableToken.symbol(),
+                            payableTokenDecimals: await payableToken.decimals(),
+                            durationSecs: loanData.terms.durationSecs,
+                            principal: loanData.terms.principal,
+                            interest: loanData.terms.interest
+                        },
+                        collateral: {
+                            erc20: erc20Holdings,
+                            erc721: erc721Holdings,
+                            erc1155: erc1155Holdings,
+                        }
+                    });
+                }
+            }
+
+            for (let i = 0; i < numLegacyLenderNotes; i++) {
+                const lenderNoteId = await legacyLenderNote.tokenOfOwnerByIndex(account, i);
+                const loanId = await legacyLenderNote.loanIdByNoteId(lenderNoteId);
+                const loanData = await legacyLoanCore.getLoan(loanId);
+
+                // Active state
+                if (loanData.state === 2) {
+                    // Find collateral
+                    const tokenId = loanData.terms.collateralTokenId;
+                    const erc20Holdings = [];
+                    const erc721Holdings = [];
+                    const erc1155Holdings = [];
+
+                    let failed = false;
+                    let i = 0;
+                    while (!failed) {
+                        try {
+                            const holding = await assetWrapper.bundleERC20Holdings(tokenId, i++);
+                            const token = new ethers.Contract(holding.tokenAddress, ERC20Abi, provider);
+                            const tokenName = await token.name();
+                            const symbol = await token.symbol();
+                            const decimals = await token.decimals();
+
+                            erc20Holdings.push({
+                                amount: holding.amount,
+                                tokenAddress: holding.tokenAddress,
+                                token,
+                                tokenName,
+                                symbol,
+                                decimals
+                            });
+                        } catch (e) {
+                            failed = true;
+                        }
+                    }
+
+                    failed = false;
+                    i = 0;
+                    while (!failed) {
+                        try {
+                            const holding = await assetWrapper.bundleERC721Holdings(tokenId, i++);
+                            const token = new ethers.Contract(holding.tokenAddress, ERC721Abi, provider);
+                            const tokenName = await token.name();
+                            const symbol = await token.symbol();
+
+                            erc721Holdings.push({
+                                tokenAddress: holding.tokenAddress,
+                                tokenId: holding.tokenId,
+                                token,
+                                tokenName,
+                                symbol
+                            });
+                        } catch (e) {
+                            failed = true;
+                        }
+                    }
+
+                    failed = false;
+                    i = 0;
+                    while (!failed) {
+                        try {
+                            const holding = await assetWrapper.bundleERC1155Holdings(tokenId, i++);
+                            const token = new ethers.Contract(holding.tokenAddress, ERC1155Abi, provider);
+                            const tokenName = await token.name();
+                            const symbol = await token.symbol();
+
+                            erc1155Holdings.push({
+                                tokenAddress: holding.tokenAddress,
+                                tokenId: holding.tokenId,
+                                amount: holding.amount,
+                                token,
+                                tokenName,
+                                symbol
+                            });
+                        } catch (e) {
+                            failed = true;
+                        }
+                    }
+
+                    const payableToken = new ethers.Contract(loanData.terms.payableCurrency, ERC20Abi, provider);
+
+                    loans.push({
+                        legacy: false,
+                        loanId: loanId,
+                        data: {
+                            state: loanData.state,
+                            dueDate: loanData.dueDate.toNumber(),
+                            borrowerNoteId: loanData.borrowerNoteId.toNumber(),
+                            lenderNoteId: loanData.lenderNoteId.toNumber()
+                        },
+                        terms: {
+                            collateralTokenId: loanData.terms.collateralTokenId,
+                            payableCurrency: loanData.terms.payableCurrency,
+                            payableToken: payableToken,
+                            payableTokenName: await payableToken.name(),
+                            payableTokenSymbol: await payableToken.symbol(),
+                            payableTokenDecimals: await payableToken.decimals(),
+                            durationSecs: loanData.terms.durationSecs,
+                            principal: loanData.terms.principal,
+                            interest: loanData.terms.interest
+                        },
+                        collateral: {
+                            erc20: erc20Holdings,
+                            erc721: erc721Holdings,
+                            erc1155: erc1155Holdings,
+                        }
+                    });
+                }
+            }
+
+            const info = {
+                account,
+                balances: {
+                    eth: ethBalance,
+                    usdc: usdcBalance,
+                    weth: wethBalance
+                },
+                loans,
+                contracts: {
+                    legacyLenderNote,
+                    lenderNote,
+                    legacyLoanCore,
+                    loanCore,
+                    assetWrapper,
+                    flashRollover
+                }
+            }
+
+            setChainInfo(Object.assign({ ...chainInfo }, info));
+        };
+
+        getInfo();
+    }, [wallet]);
+
+    // const ethBalance = await ethers.provider.getBalance()
+
+    // For each loan, get all owned assets
+
+
+    // Return active loans
+    // Return contract addresses
+    return chainInfo;
+}
